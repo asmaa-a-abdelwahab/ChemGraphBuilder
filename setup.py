66a05cd4b2c769554c35a4808eb682eb296dcc96
asmaa-a-abdelwahab <asmaa.a.abdelwahab@gmail.com>
2024-07-20 20:19:57 +0000

    Update Scripts&Examples

--- setup.py
+++ setup.py
@@ -10,6 +10,9 @@ setup(
     ],
     entry_points={
         'console_scripts': [
+            'setup-data-folder=chemgraphbuilder.setup_data_folder:main'
+            'collect-process-nodes=chemgraphbuilder.node_collector_processor:main',
+            'collect-process-relationships=chemgraphbuilder.relationship_collector_processor:main',
             'load-graph-nodes=chemgraphbuilder.graph_nodes_loader:main',
             'load-graph-relationships=chemgraphbuilder.graph_relationships_loader:main',
         ],
