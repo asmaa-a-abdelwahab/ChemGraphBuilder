f3cf40baa625a7829c38c3c580fa39d96aa73a2c
asmaa-a-abdelwahab <asmaa.a.abdelwahab@gmail.com>
2024-07-28 18:33:29 +0000

    test setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='chemgraphbuilder',
-    version='0.1.2',
+    version='0.2.0',
     packages=find_packages(),
     install_requires=[
         "beautifulsoup4",
